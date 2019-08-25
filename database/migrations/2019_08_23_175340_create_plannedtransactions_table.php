5b9063dd29ecb7e1550cf83836d0ef17791726da
Michael Claybaugh <michael.claybaugh@gmail.com>
2019-08-25 07:43:41 -0400

    remove frequencies, add transactions

--- database/migrations/2019_08_23_175340_create_plannedtransactions_table.php
+++ database/migrations/2019_08_23_175340_create_plannedtransactions_table.php
@@ -5,32 +5,31 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class PlannedTransaction extends Migration {
+
   /**
    * Run the migrations.
    *
-   * @return void
+   * If interval_days is 0, then transaction is monthly.
    */
   public function up() {
-    Schema::create('frequencies', function (Blueprint $table) {
-      $table->increments('id');
-      $table->string('name');
-    });
     Schema::create('plannedtransactions', function (Blueprint $table) {
-      $table->increments('id');
+      $table->bigIncrements('id');
       $table->timestamps();
+      $table->foreign('user_id')
+        ->references('id')->on('users')
+        ->onDelete('cascade');
       $table->text('description');
       $table->decimal('amount', 8, 2);
-      // user
-      // frequency
+      $table->dateTime('datetime');
+      $table->integer('interval_days');
     });
   }
 
   /**
    * Reverse the migrations.
-   *
-   * @return void
    */
   public function down() {
     Schema::dropIfExists('plannedtransactions');
   }
+
 }
