ddf992c86870425944ad33de45626929cecac9b6
Michael Claybaugh <michael.claybaugh@gmail.com>
2019-08-26 13:33:38 -0400

    vue, tachyons, style login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,25 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+class HomeController extends Controller {
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
+  /**
+   * Create a new controller instance.
+   */
+  public function __construct() {
+    $this->middleware('auth');
+  }
+
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   *   Homepage.
+   */
+  public function index() {
+    return view('home');
+  }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<article class="center mw5 mw6-ns br3 hidden ba b--gray mv4">
+  <h2 class="f4 bg-near-black br3 br--top black-60 mv0 pv2 ph3 silver">Title of card</h1>
+  <div class="pa3 bt b--gray">
+    @if (session('status'))
+      <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+      </div>
+    @endif
+    <p class="f6 f5-ns lh-copy measure silver">
+        You are logged in!
+    </p>
+  </div>
+</article>
 @endsection
