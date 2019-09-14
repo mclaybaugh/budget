35bb27aad03c8c15bf32a37f310b63f6847ba41f
Michael Claybaugh <michael.claybaugh@gmail.com>
2019-09-14 15:15:13 -0400

    template table add caption

--- resources/views/components/table_template.blade.php
+++ resources/views/components/table_template.blade.php
@@ -6,12 +6,13 @@
 --}}
 <div class="overflow-auto pa3">
   <table class="f6 w-100 depth-1 collapse text-1">
+  <caption>{{ $title }}</caption>
     <colgroup>
       <col span="1" style="width: 50%">
       <col span="1" style="width: 25%">
       <col span="1" style="width: 25%">
     </colgroup>
-    <thead>
+    <thead class="dn">
       <tr>
         <th class="text-0 fw6 tc pa3 bb b--dark-gray">Description</th>
         <th class="text-0 fw6 tc pa3 bb b--dark-gray">Date</th>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,13 +5,17 @@ Template |
 @endsection
 
 @section('content')
-@foreach ($data as $cat => $records)
-  @component('components.template_category', [
-    'title' => $cat,
-    'rows' => $records,
-  ])
-  @endcomponent
-@endforeach
+<div class="flex-l justify-center">
+  @foreach ($data as $cat => $records)
+    @if (count($records) > 0)
+      @component('components.table_template', [
+        'title' => $cat,
+        'rows' => $records,
+      ])
+      @endcomponent
+    @endif
+  @endforeach
+</div>
 
 <div class="center mw5 ba b--primary primary bg-transparent f6">
   <a class="link dim db primary tc b ph3 pv2"
