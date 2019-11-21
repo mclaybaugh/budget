0965d04f4ebab444080296742fbe204b7947fac8
Michael Claybaugh <michael.claybaugh@gmail.com>
2019-11-21 08:38:18 -0500

    finish styling auth views

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,47 +3,35 @@
 Forgot Password
 @endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@component('components.form', [
+  'method' => 'POST',
+  'route' => route('password.email'),
+])
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+  @if (session('status')) {{ session('status') }} @endif
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+  @component('components.fieldset', [
+    'title' => 'Reset Password',
+  ])
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    @component('components.input', [
+      'short' => 'email',
+      'display' => 'Email Address',
+      'type' => 'email',
+      'value' => old('email'),
+      'required' => TRUE,
+      'autocomplete' => TRUE,
+      'autofocus' => TRUE,
+    ])
+    @endcomponent
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    @component('components.ghost_btn', [
+      'text' => 'Send Password Reset Link',
+    ])
+    @endcomponent
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  @endcomponent {{-- fieldset --}}
+
+@endcomponent {{-- form --}}
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -3,65 +3,55 @@
 Password Reset
 @endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+@component('components.form', [
+  'method' => 'POST',
+  'route' => route('password.update'),
+])
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+  @if (session('status')) {{ session('status') }} @endif
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+  @component('components.fieldset', [
+    'title' => 'Reset Password',
+  ])
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+    <input type="hidden" name="token" value="{{ $token }}">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    @component('components.input', [
+      'short' => 'email',
+      'display' => 'Email Address',
+      'type' => 'email',
+      'value' => $email ?? old('email'),
+      'required' => TRUE,
+      'autocomplete' => TRUE,
+      'autofocus' => TRUE,
+    ])
+    @endcomponent
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+    @component('components.input', [
+      'short' => 'password',
+      'display' => 'Password',
+      'type' => 'password',
+      'required' => TRUE,
+      'autocomplete' => TRUE,
+    ])
+    @endcomponent
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+    @component('components.input', [
+      'short' => 'password_confirmation',
+      'display' => 'Confirm Password',
+      'type' => 'password',
+      'required' => TRUE,
+      'autocomplete' => TRUE,
+    ])
+    @endcomponent
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    @component('components.ghost_btn', [
+      'text' => 'Reset Password',
+    ])
+    @endcomponent
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+  @endcomponent {{-- fieldset --}}
+@endcomponent {{-- form --}}
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
