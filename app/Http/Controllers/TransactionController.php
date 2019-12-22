75481ed428f2b913a37b7fa48e4a45af1b7696ce
Michael Claybaugh <michael.claybaugh@gmail.com>
2019-12-22 17:07:04 -0500

    transaction highlight today

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -53,8 +53,10 @@ class TransactionController extends Controller {
       $balance = $beginningBalance;
     }
     $rows = [];
+    $today = date('Y-m-d');
     while ($day < $nextMonth) {
       $nextDay = strtotime('+1 day', $day);
+      $dayDate = date('Y-m-d', $day);
       $dayTransactions = Transaction::where('user_id', Auth::id())
         ->where('datetime', '>=', date('Y-m-d H:i:s', $day))
         ->where('datetime', '<', date('Y-m-d H:i:s', $nextDay))
@@ -67,12 +69,17 @@ class TransactionController extends Controller {
         } else {
           $balanceValue = '#';
         }
+        $isToday = FALSE;
+        if ($today === $dayDate) {
+          $isToday = TRUE;
+        }
         $rows[] = [
           'date' => date('j', $day),
           'amount' => '-',
           'balance' => $balanceValue,
           'description' => '-',
           'edit_link' => route('transaction.create'),
+          'is_today' => $isToday,
         ];
       } else {
         $i = 0;
@@ -83,13 +90,22 @@ class TransactionController extends Controller {
           } else {
             $balanceValue = '#';
           }
-          $date = $i === 0 ? date('j', strtotime($transaction->datetime)) : '';
+          $isToday = FALSE;
+          if ($i === 0) {
+            $date = date('j', $day);
+            if ($today === $dayDate) {
+              $isToday = TRUE;
+            }
+          } else {
+            $date = '';
+          }
           $rows[] = [
             'date' => $date,
             'amount' => number_format($transaction->amount, 2),
             'balance' => $balanceValue,
             'description' => $transaction->description,
             'edit_link' => route('transaction.edit', $transaction->id),
+            'is_today' => $isToday,
           ];
           $i++;
         }
--- resources/views/components/table_transaction.blade.php
+++ resources/views/components/table_transaction.blade.php
@@ -17,7 +17,9 @@
     </thead>
     <tbody>
       @foreach($rows as $row)
-        <tr @if ($loop->even) class="depth-2" @endif>
+        <tr @if ($row['is_today']) class="bg-primary900"
+          @elseif ($loop->even) class="depth-2"
+          @endif>
           <td class="ph3 pv1">{{ $row['date'] }}</td>
           <td class="text-1 ph3 pv1 tr">{{ $row['amount'] }}</td>
           <td class="text-1 ph3 pv1 tr">{{ $row['balance'] }}</td>
