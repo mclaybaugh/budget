36ed4aaf5838a84d1a574d7873755b7c435488ce
Michael Claybaugh <michael.claybaugh@gmail.com>
2020-05-01 10:48:21 -0400

    extraClasses fix

--- resources/views/components/ghost_btn.blade.php
+++ resources/views/components/ghost_btn.blade.php
@@ -4,6 +4,6 @@
   - text
   - extraClasses
 --}}
-<button class="{{ $extraClasses }} link dim db primary tc b ph3 pv2 ba b--primary primary bg-transparent f6 pointer">
+<button class="@isset($extraClasses){{ $extraClasses }}@endisset link dim db primary tc b ph3 pv2 ba b--primary primary bg-transparent f6 pointer">
   {{ $text }}
 </button>
